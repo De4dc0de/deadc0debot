8282fc982f82b25f58efa9665703b652dc7f7247
waweic <hesiha@gmx.de>
2017-03-15 18:42:03 +0100

    Update handlefile.py

--- handlefile.py
+++ handlefile.py
@@ -5,15 +5,16 @@ def handle(msg, bot, reimport):
     #id = msg["from"]["id"]
     if("entities" in msg and msg["entities"][0]["type"] == "bot_command"):
         #bot.sendMessage(id, "Command recognized, but not yet supported")
-        if(msg["text"][0] == "/"):
-            if(msg["text"] == "/help"):
+        command = msg["text"].lower()
+        if(command[0] == "/"):
+            if(command == "/help"):
                 bot.sendMessage(id, """These are the commands, we actually have:
                 /help to show this
                 /reload to get the newest handling file
                 /tutorial to learn hacking """)
-            elif(msg["text"] == "/reload"):
+            elif(command == "/reload"):
                 return reimport(id)
-            elif(msg["text"] == "/tutorial"):
+            elif(command == "/tutorial"):
                 bot.sendMessage(id, open("tutorial.txt").read())
             else:
                 bot.sendMessage(id, "Sorry, no recognizeable command. Use /help instead")
