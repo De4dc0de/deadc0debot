e909470eff460da334722d1ad395b88d634bd80e
waweic <hesiha@gmx.de>
2017-03-15 20:25:50 +0100

    Update handlefile.py
    Added /voteban

--- handlefile.py
+++ handlefile.py
@@ -1,22 +1,26 @@
 def handle(msg, bot, reimport):
+    import pickle
     botid = "@Deadc0deBot"
     botid = botid.lower()
+    banamount = 10
     #print("reloaded")
     #pprint(msg)
     id = msg["chat"]["id"]
     #id = msg["from"]["id"]
     if("entities" in msg and msg["entities"][0]["type"] == "bot_command"):
         #bot.sendMessage(id, "Command recognized, but not yet supported")
-        command = msg["text"].lower()
+        realcommand = msg["text"]
+        command = msg["text"].lower().split(" ")[0]
         print(command)
         if(command[0] == "/"):
             if(command == "/help"):
                 bot.sendMessage(id, """These are the commands, we actually have:
-                /help to show this
-                /reload to get the newest handling file
-                /tutorial to learn hacking
-                /source Show a Link to the source of the bot
-                /web Show a Link to the Web page""")
+                /help - show this
+                /reload - get the newest handling file
+                /tutorial - learn hacking
+                /source - show a Link to the source of the bot
+                /web - show a Link to the Web page
+                /voteban @user - be evil and ban an evil user""")
             elif(command == "/reload" or command == "/reload" + botid):
                 return reimport(id)
             elif(command == "/tutorial" or command == "/tutorial" + botid):
@@ -25,6 +29,24 @@ def handle(msg, bot, reimport):
                 bot.sendMessage(id, "https://github.com/De4dc0de/deadc0debot")
             elif(command == "/web" or command == "/web" + botid):
                 bot.sendMessage(id, "https://http://deadc0de.bplaced.net")
+            elif(command == "/voteban" or command == "/voteban" + botid):
+                try:
+                    bandict = pickle.load(open("bandict"))
+                except:
+                    bandict = {}
+                try:
+                    banuser = realcommand.split(" ")[1]
+                    if(banuser != 0):
+                        if(not banuser in bandict):
+                            bandict[banuser] = 1
+                        else:
+                            bandict[banuser] = bandict[banuser] + 1
+                        lastvote = banuser
+                        bot.sendMessage(id, "Voteban " + banuser + " " + str(bandict[banuser]) + "/" + str(banamount))
+                except:
+                    bandict[banuser] = bandict[banuser] + 1
+                    bot.sendMessage(id, "Voteban " + banuser + " " + str(bandict[banuser]) + "/" + str(banamount))
+                pickle.dump(bandict, open("bandict"))
             else:
                 bot.sendMessage(id, "Sorry, no recognizeable command. Use /help instead")
                 #bot.sendMessage(id, raw_input(msg["text"] + ": "))
