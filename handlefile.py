3789146771aac036888dee984d429980c39771ff
waweic <hesiha@gmx.de>
2017-03-15 18:27:25 +0100

    Update handlefile.py
    Changed ids

--- handlefile.py
+++ handlefile.py
@@ -1,21 +1,23 @@
 def handle(msg, bot, reimport):
     #print("reloaded")
     #pprint(msg)
+    id = msg["chat"]["id"]
+    #id = msg["from"]["id"]
     if("entities" in msg and msg["entities"][0]["type"] == "bot_command"):
-        #bot.sendMessage(msg["from"]["id"], "Command recognized, but not yet supported")
+        #bot.sendMessage(id, "Command recognized, but not yet supported")
         if(msg["text"][0] == "/"):
             if(msg["text"] == "/help"):
-                bot.sendMessage(msg["from"]["id"], """These are the commands, we actually have:
+                bot.sendMessage(id, """These are the commands, we actually have:
                 /help to show this
                 /reload to get the newest handling file
                 /tutorial to learn hacking """)
             elif(msg["text"] == "/reload"):
-                return reimport(msg["from"]["id"])
+                return reimport(id)
             elif(msg["text"] == "/tutorial"):
-                bot.sendMessage(msg["from"]["id"], open("tutorial.txt").read())
+                bot.sendMessage(id, open("tutorial.txt").read())
             else:
-                bot.sendMessage(msg["from"]["id"], "Sorry, no recognizeable command. Use /help instead")
-                #bot.sendMessage(msg["from"]["id"], raw_input(msg["text"] + ": "))
+                bot.sendMessage(id, "Sorry, no recognizeable command. Use /help instead")
+                #bot.sendMessage(id, raw_input(msg["text"] + ": "))
             
     else:
         pass
