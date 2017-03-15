797e21d6d85211963da5ca0ba1dacc261ce933b6
waweic <hesiha@gmx.de>
2017-03-15 16:56:27 +0100

    Update bot.py
    Added exception handling

--- bot.py
+++ bot.py
@@ -9,12 +9,18 @@ from pprint import pprint
 bot = telepot.Bot(configfile.id)
 
 def reimport(id):
-    os.system("wget https://raw.githubusercontent.com/De4dc0de/deadc0debot/master/handlefile.py -O handlefile.py")
-    reload(handlefile)
-    print("reloaded")
-    bot.sendMessage(id, "done")
+    try:
+        os.system("wget https://raw.githubusercontent.com/De4dc0de/deadc0debot/master/handlefile.py -O handlefile.py")
+        reload(handlefile)
+        print("reloaded")
+        bot.sendMessage(id, "done")
+    except:
+        bot.sendMessage(id, "Update failed. Kick the Bot")
 def handle(msg):
-    handlefile.handle(msg, bot, reimport)
+    try:
+        handlefile.handle(msg, bot, reimport)
+    except:
+        bot.sendMessage(msg["from"]["id"], "Launch failed. Kick the Bot and don't write anything")
 
 bot.message_loop(handle)
 
