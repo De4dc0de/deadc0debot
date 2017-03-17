2ece12c3579a66513156f35b8e7622130cc02e3b
waweic <spamstoppp@gmx.de>
2017-03-17 11:33:23 +0100

    did changes

---
+++ jokes.py
@@ -0,0 +1,5 @@
+jokes = ["Not funny", 
+    "Der groesste Witz bist du!",
+    "Wie viele Neger braucht man, um ein Zimmer zu putzen? Keine! Das ist Frauenarbeit!",
+    "Hitler",
+    "Wenn mir meine Frau kein Brot machen will: -Sudo make me a sandwich- "]
