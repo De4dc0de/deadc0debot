ec308614599b94c8c92083f21b95a8bc4db6f393
waweic <hesiha@gmx.de>
2017-03-20 20:48:35 +0100

    Update and rename jokes.py to jokes.txt

--- jokes.py
+++
@@ -1,7 +0,0 @@
-jokes = ["Not funny", 
-    "Der groesste Witz bist du!",
-    "Wie viele Neger braucht man, um ein Zimmer zu putzen? Keine! Das ist Frauenarbeit!",
-    "Hitler",
-    "Frauenrechte",
-    "Gute Nachricht: Ich bekomme endlich den obersten Knopf meiner superengen Jeans zu-Schlechte Nachricht: Habe sie leider nicht an.",
-    "Wenn mir meine Frau kein Brot machen will: -Sudo make me a sandwich- "]
