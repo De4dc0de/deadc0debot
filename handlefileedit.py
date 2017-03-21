ca34ce2e2e05fd1401f1ab948ef8543e84e6055f
waweic <hesiha@gmx.de>
2017-03-21 12:43:19 +0100

    Update handlefileedit.py

--- handlefileedit.py
+++ handlefileedit.py
@@ -123,4 +123,4 @@ def handle(msg, bot, reimport):
             if("xD" in msg["text"] or "XD" in msg["text"] or "xd" in msg["text"] or "Xd" in msg["text"]):
                 bot.sendMessage(id, u'\U0001f606')
         except:
-pass
+            pass
