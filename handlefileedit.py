739c9c46b078d83e62981c1ff012932966e37306
waweic <hesiha@gmx.de>
2017-03-22 16:35:17 +0100

    Update handlefileedit.py
    Added omivoren Text

--- handlefileedit.py
+++ handlefileedit.py
@@ -72,6 +72,8 @@ def handle(msg, bot, reimport):
                     bot.sendMessage(id, "<====3")
             elif(command == "/love" or command == "/love" + botid):
                 bot.sendMessage(id, "<3")
+            elif(command == "/omivoren" or command == "/omnivoren" + botid):
+                bot.sendMessage(id, "Jedes Stück Fleisch war mal ein Tier!")
             elif(command == "/web" or command == "/web" + botid):
                 try:
                     bot.sendMessage(id, configfile.weburl)
